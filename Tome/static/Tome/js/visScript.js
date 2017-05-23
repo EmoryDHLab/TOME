64788b9a3ec3cc7e860be259f19371f949b54538
Adam Hayward <achayward1@gmail.com>
2017-05-23 14:49:17 -0400

    began work on scaling chart on slider change

--- Tome/static/Tome/js/visScript.js
+++ Tome/static/Tome/js/visScript.js
@@ -9,54 +9,60 @@ for ( var i = 0; i < 100; i++) {
 }
 var height = 850,
     width = 850,
-    offset = 3;
+    offset = 3
+    heightOut = 0,
+    widthOut = 0;
 
 var getRectWidth = function() {
-  return (width - (99 * offset))/100;
+  console.log(widthOut);
+  return (width - ((99 - widthOut) * offset)) / (100 - widthOut);
 }
 var getRectHeight = function() {
-  return (height - (99 * offset))/100;
+  console.log(heightOut);
+  return (height - ((99 - heightOut) * offset)) / (100 - heightOut);
 }
 
-var updateCorpusChart = function(val, isVert, isMax) {
-  console.log("--------" + val + "-------");
-  // val is 0-99 for slider position
-  // if it is horiz, then select by data-j
-  var iOrJ = (isVert) ? "i" : "j";
+var resizeCorpusChart = function() {
+  d3.selectAll("#corpus-chart rect")
+    .attr("width", getRectWidth())
+    .attr("height", getRectHeight())
+    .attr("transform", "translate(" + corpusSliders.x.getShift()
+      + ", " + corpusSliders.y.getShift() + ")")
+}
+
+var updateCorpusChart = function() {
+  console.log(corpusSliders);
+  heightOut = 0;
+  widthOut = 0;
+  $(".out").removeClass("out");
   // if it is vert, then select by data-i
-  $('rect[data-' + iOrJ + ']').filter(function () {
-    if (isMax) {
-      // if it is max, then select all < val
-      if (isVert) {
-        return $(this).data(iOrJ) < val;
-      } else {
-        return $(this).data(iOrJ) > val;
-      }
-    } else {
-      // if it is min, then select all > val
-      if (isVert) {
-        return $(this).data(iOrJ) > val;
-      } else {
-        return $(this).data(iOrJ) < val;
-      }
+  $('rect[data-j]').filter(function () {
+    var vx = false, vn = false, hx = false, hn = false;
+    var yval = $(this).data('i');
+    var xval = $(this).data('j');
+    if (yval > corpusSliders.y.minVal) {
+      vn = true;
     }
-  }).css("opacity",".1");
-  $('rect[data-' + iOrJ + ']').filter(function () {
-    if (isMax) {
-      if (isVert) {
-        return $(this).data(iOrJ) >= val;
-      } else {
-        return $(this).data(iOrJ) <= val;
-      }
-    } else {
-      // if it is min, then select all > val
-      if (isVert) {
-        return $(this).data(iOrJ) <= val;
-      } else {
-        return $(this).data(iOrJ) >= val;
-      }
+    if (yval < corpusSliders.y.maxVal) {
+      vx = true;
     }
-  }).css("opacity","1");
+    if (xval > corpusSliders.x.maxVal) {
+      hx = true;
+    }
+    if (xval < corpusSliders.x.minVal) {
+      hn = true;
+    }
+    if (vn || vx) {
+      heightOut++;
+    }
+    if (hn || hx) {
+      widthOut++;
+    }
+    return vn || vx || hn || hx;
+  }).addClass("out");
+  widthOut/=100;
+  heightOut/=100;
+  resizeCorpusChart();
 }
 
 var myChart = d3.select('#corpus-chart').append('svg')
@@ -130,7 +136,8 @@ function appendSlider(selector, vertical = false) {
     .style(styles.thick, sThickness + 'px');
 
   var sliderHandleMax = slider.append("div")
-    .attr('class', 'slider-handle max-handle');
+    .attr('class', 'slider-handle max-handle')
+    .attr('data-value', (vertical) ? 0 : 99);
 
   if (vertical) {
     sliderHandleMax.style("top","0");
@@ -139,7 +146,8 @@ function appendSlider(selector, vertical = false) {
   }
 
   var sliderHandleMin = slider.append("div")
-    .attr('class', 'slider-handle min-handle');
+    .attr('class', 'slider-handle min-handle')
+    .attr('data-value', (vertical) ? 99 : 0);
   sliderHandleMax.append("div")
     .attr("class", "slider-handle-icon")
   sliderHandleMin.append("div")
@@ -172,41 +180,62 @@ function appendSlider(selector, vertical = false) {
 dispatch.on('maxChange', function(target, value) {
   value = Math.round(value);
   coreVal = value;
-  console.log(value);
   value = sY(value);
+  d3.select(target).attr('data-value', coreVal);
   var p = d3.select(target.parentNode);
   var min = p.select('.min-handle');
   if (p.classed('vertical')) {
     if (value < parseInt(min.style('top').replace("px",""))) {
+      corpusSliders.y.maxVal = coreVal;
       d3.select(target).style('top', Math.round(value) + "px")
-      updateCorpusChart(coreVal, true, true);
+      updateCorpusChart();
     }
   } else {
     if (value > parseInt(min.style('left').replace("px",""))) {
+      corpusSliders.x.maxVal = coreVal;
       d3.select(target).style('left', Math.round(value) + "px")
-      updateCorpusChart(coreVal, false, true);
+      updateCorpusChart();
     }
   }
 });
 dispatch.on('minChange', function(target, value) {
   value = Math.round(value);
   coreVal = value;
-  console.log(value);
   value = sY(value);
+  d3.select(target).attr('data-value', coreVal);
   var p = d3.select(target.parentNode);
   var max = p.select('.max-handle');
   if (p.classed('vertical')) {
     if (value > parseInt(max.style('top').replace("px",""))) {
+      corpusSliders.y.minVal = coreVal;
       d3.select(target).style('top', Math.round(value) + "px")
-      updateCorpusChart(coreVal, true, false);
+      updateCorpusChart();
     }
   } else {
     if (value < parseInt(max.style('left').replace("px",""))) {
+      corpusSliders.x.minVal = coreVal;
       d3.select(target).style('left', Math.round(value) + "px")
-      updateCorpusChart(coreVal, false, false);
+      updateCorpusChart();
     }
   }
 });
+
+var corpusSliders = {
+  x:{
+      maxVal: 99,
+      minVal: 0,
+      getShift: function() {
+        return -(this.minVal + 99 - this.maxVal) * (getRectWidth() + offset);
+      }
+    },
+  y:{
+      maxVal:0,
+      minVal:99,
+      getShift: function() {
+        return -(this.maxVal + 99 - this.minVal) * (getRectWidth() + offset);
+      }
+    }
+}
 
 appendSlider("#vertical-slide", true);
 appendSlider("#horizontal-slide");
