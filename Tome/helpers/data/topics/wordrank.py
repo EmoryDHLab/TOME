798444939cae4238995afd16735d27278c26ec40
Adam Hayward <achayward1@gmail.com>
2018-05-23 13:04:11 -0400

    word ranks generator fix

--- Tome/helpers/data/topics/wordrank.py
+++ Tome/helpers/data/topics/wordrank.py
@@ -1,35 +1,45 @@
 from topics.models import WordTopicRank, Topic
 from Tome.helpers.debug import Printer
+from django.db import IntegrityError, transaction
 
 out = Printer()
 
 
 def wipeWordRanks():
-    WordTopicRank.objects.all().update(rank=0)
+    WordTopicRank.objects.all().update(rank=-1)
 
 
 def generateRanks():
     topics = Topic.objects.all()
+    i = 0
     for topic in topics:
         wtrs = topic.wordtopicrank_set.order_by('-score')
-        i = 0
-        for wtr in wtrs:
-            wtr.rank = i
-            wtr.save()
-            if (i % 100 == 0):
-                out.log("{}".format(i))
-            i += 1
+        try:
+            with transaction.atomic():
+                for wtr in wtrs:
+                    wtr.rank = i
+                    wtr.save()
+                    if (i % 1000 == 0):
+                        out.log("{}/".format(i))
+                    i += 1
+        except IntegrityError:
+            wipeWordRanks()
 
 
-def qRun():
+def validateWordRanks():
+    return WordTopicRank.objects.filter(rank=-1).count() == 0
+
+
+def main():
     out.log('Clearing ranks')
     wipeWordRanks()
     out.log('Generating word ranks')
     generateRanks()
-
-
-def main():
-    qRun()
+    out.log('Validating...')
+    success = validateWordRanks()
+    if not success:
+        raise Exception("Ranks were not generated reliably")
+    out.log('Done.')
 
 
 if __name__ == '__main__':
